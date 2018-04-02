0f45396da7b225a8cceeed452ad9ec2e047c6816
wy5622150 <wy5622150@163.com>
2018-04-01 21:12:51 -0400

    modified

--- src/App.test.js
+++ src/App.test.js
@@ -9,31 +9,31 @@ import Adapter from 'enzyme-adapter-react-16';
 import { expect } from 'chai'
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('组件正确渲染', function () {
-  it('App正确渲染', () => {
+describe('Components render properly', function () {
+  it('App renders correctly', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
-  it('output1模块正确渲染', () => {
+  it('output1 renders correctly', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Output1 content={[]} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
-  it('output2模块正确渲染', () => {
+  it('output2 renders correctly', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Output2 content={[]} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
-  it('output3模块正确渲染', () => {
+  it('output3 renders correctly', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Output3 content={[]} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 })
-describe('函数运行正常(每个函数9个测试点)', function () {
+describe('Functions perform well(9 checking items for each function)', function () {
   var target = []
-  it('DataTrans()转化功能正常', function () {
+  it('DataTrans() performs well', function () {
     var commaRaw = 'Abercrombie, Neil, Male, Tan, 2/13/1943\nBishop, Timothy, Male, Yellow, 4/23/1967\nKelly, Sue, Female, Pink, 7/12/1959'.split('\n'),
       pipeRaw = 'Smith | Steve | D | M | Red | 3-3-1985\nBonk | Radek | S | M | Green | 6-3-1975\nBouillon | Francis | G | M | Blue | 6-3-1975'.split('\n'),
       spaceRaw = 'Kournikova Anna F F 6-3-1975 Red\nHingis Martina M F 4-2-1979 Green\nSeles Monica H F 12-2-1973 Black'.split('\n')
@@ -48,7 +48,7 @@ describe('函数运行正常(每个函数9个测试点)', function () {
     expect(target[7][0]).to.equal('Hingis')
     expect(target[8][0]).to.equal('Seles')
   })
-  it('Output1组件itemSortJoin()功能正常', function () {
+  it('Output1.itemSortJoin() performs well', function () {
     expect(Output1.prototype.itemSortJoin(target)[0]).to.equal('Hingis Martina Female 4/2/1979 Green')
     expect(Output1.prototype.itemSortJoin(target)[1]).to.equal('Kelly Sue Female 7/12/1959 Pink')
     expect(Output1.prototype.itemSortJoin(target)[2]).to.equal('Kournikova Anna Female 6/3/1975 Red')
@@ -59,7 +59,7 @@ describe('函数运行正常(每个函数9个测试点)', function () {
     expect(Output1.prototype.itemSortJoin(target)[7]).to.equal('Bouillon Francis Male 6/3/1975 Blue')
     expect(Output1.prototype.itemSortJoin(target)[8]).to.equal('Smith Steve Male 3/3/1985 Red')
   })
-  it('Output2组件itemSortJoin()功能正常', function () {
+  it('Output2.itemSortJoin() performs well', function () {
     expect(Output2.prototype.itemSortJoin(target)[0]).to.equal('Abercrombie Neil Male 2/13/1943 Tan')
     expect(Output2.prototype.itemSortJoin(target)[1]).to.equal('Kelly Sue Female 7/12/1959 Pink')
     expect(Output2.prototype.itemSortJoin(target)[2]).to.equal('Bishop Timothy Male 4/23/1967 Yellow')
@@ -70,7 +70,7 @@ describe('函数运行正常(每个函数9个测试点)', function () {
     expect(Output2.prototype.itemSortJoin(target)[7]).to.equal('Hingis Martina Female 4/2/1979 Green')
     expect(Output2.prototype.itemSortJoin(target)[8]).to.equal('Smith Steve Male 3/3/1985 Red')
   })
-  it('Output3组件itemSortJoin()功能正常', function () {
+  it('Output3.itemSortJoin() performs well', function () {
     expect(Output3.prototype.itemSortJoin(target)[0]).to.equal('Smith Steve Male 3/3/1985 Red')
     expect(Output3.prototype.itemSortJoin(target)[1]).to.equal('Seles Monica Female 12/2/1973 Black')
     expect(Output3.prototype.itemSortJoin(target)[2]).to.equal('Kournikova Anna Female 6/3/1975 Red')
